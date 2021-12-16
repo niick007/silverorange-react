2b024c5752d996c25d2ecb59676daa44c7d0a324
nachiketpanchal <nnpanchal007@gmail.com>
2021-12-16 01:45:38 -0400

    Aggrigated git repos with local json in API

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,10 +1,26 @@
+import axios from 'axios';
 import { Router, Request, Response } from 'express';
-import data from '../../data/repos.json';
+import localData from '../../data/repos.json';
 export const repos = Router();
 
 repos.get('/', async (req: Request, res: Response) => {
+  const data = localData.filter((el) => el.fork === false);
+  let responseData: any = [];
+  let statusCode = 200;
+  await axios
+    .get('https://api.github.com/users/silverorange/repos')
+    .then(function (response) {
+      const gitData = response.data.filter(
+        (el: { fork: boolean }) => el.fork === false
+      );
+      responseData = data.concat(gitData);
+    })
+    .catch(function (error) {
+      statusCode = 500;
+      responseData = { message: 'Could not fetch the git repos' };
+    });
   res.header('Cache-Control', 'no-store');
   res.header('Content-Type', 'application/json');
-  res.status(200);
-  res.json(data.filter((el) => el.fork === false));
+  res.status(statusCode);
+  res.json(responseData);
 });
